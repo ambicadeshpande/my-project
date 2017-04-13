4a5ee409e7220e6a515f25581841db0ea17d5281
ambicadeshpande <ambicadeshpande@gmail.com>
2017-04-14 04:51:40 +0530

    playing with colors plus jquery

---
+++ color_change.js
@@ -0,0 +1,58 @@
+$(document).ready(function () {
+  $(".bgc_1").click(function () {
+    $(".left_col").css("background-color","#eed2ee")
+  })
+})
+$(document).ready(function () {
+  $(".bgc_2").click(function () {
+    $(".left_col").css("background-color","#dcd0c0")
+  })
+})
+$(document).ready(function () {
+  $(".bgc_3").click(function () {
+    $(".left_col").css("background-color","#bfd8d2")
+  })
+})
+$(document).ready(function () {
+  $(".bgc_4").click(function () {
+    $(".left_col").css("background-color","#b4dbc0")
+  })
+})
+$(document).ready(function () {
+  $(".tc_1").click(function () {
+    $("#p1").css("color","#373737")
+  })
+})
+$(document).ready(function () {
+  $(".tc_2").click(function () {
+    $("#p1").css("color","#cc4e5c")
+  })
+})
+$(document).ready(function () {
+  $(".tc_3").click(function () {
+    $("#p1").css("color","#4f2f4f")
+  })
+})
+$(document).ready(function () {
+  $(".tc_4").click(function () {
+    $("#p1").css("color","#4d4d4d")
+  })
+})
+
+$(document).ready(function () {
+  $(".small_btn").click(function () {
+    $("#p1").css("font-size","12px")
+  })
+})
+
+$(document).ready(function () {
+  $(".medium_btn").click(function () {
+    $("#p1").css("font-size","20px")
+  })
+})
+
+$(document).ready(function () {
+  $(".large_btn").click(function () {
+    $("#p1").css("font-size","30px")
+  })
+})
